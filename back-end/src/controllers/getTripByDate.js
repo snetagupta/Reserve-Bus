f911e98a211fdea22d46ce8da3b0cc25fe840647
Sneta Gupta <snetagupta4nov@gmail.com>
2023-12-11 10:05:16 +0530

    Changes in API

--- back-end/src/controllers/getTripByDate.js
+++ back-end/src/controllers/getTripByDate.js
@@ -7,17 +7,9 @@ const getTripByDate = async (req, res) => {
     if (!date) {
       return res.status(400).json({ error: "Date parameter is required" });
     }
-
-    // Convert the date parameter to a JavaScript Date object
-    // const tripDate = new Date(date);
-
     // Fetch trips for the specified date
     const trips = await Trip.find({
       date:date
-      // date: {
-      //   $gte: tripDate,
-      //   $lt: new Date(tripDate.getTime() + 24 * 60 * 60 * 1000),
-      // },
     });
     if (trips.length < 1) {
       return res.status(404).json({ error: "Data not found" });
